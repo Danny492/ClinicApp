f133d93f97f3931526020c76c63f55e138e15647
Danny Henriquez <dh8054302@gmail.com>
2025-09-10 21:46:54 -0400

    Proyecto base

---
+++ src/app/pages/cita/cita.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cita',
+  imports: [],
+  templateUrl: './cita.html',
+  styleUrl: './cita.scss'
+})
+export class Cita {
+
+}
